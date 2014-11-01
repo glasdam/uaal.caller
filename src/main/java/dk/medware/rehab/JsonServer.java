90c4fe5f3ccd9372f877ac2f259828dd4b540ed8
Jacob Glasdam <jacob@medware.dk>
2014-11-01 15:33:15 +0100

    Now it works, hurra

--- src/main/java/dk/medware/rehab/JsonServer.java
+++ src/main/java/dk/medware/rehab/JsonServer.java
@@ -13,14 +13,11 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
-import static java.util.Arrays.asList;
-
 public class JsonServer {
 
-	static String API_KEY = "45603837-8D85-4F27-B73C-2EC481007F36";
-	static String BASE_URL = "http://fys.medware.dk/uaal_api/evaluations";
-
 	public static void main(String[] args) throws ClientProtocolException, IOException {
+		System.setProperty("rehab.server.api", "api-key");
+		System.setProperty("rehab.server.url", "url");
 		List<Long> ids = list_needs_evaluation(); 
 		puts(ids);
 		for(long id : ids){
@@ -33,7 +30,7 @@ public class JsonServer {
 			//send_to_server(id, results);
 		}
 	}
-	
+/*	
 	public static void service_server() throws ClientProtocolException, IOException{
 		List<Long> ids = list_needs_evaluation(); 
 		//puts(ids);
@@ -41,19 +38,19 @@ public class JsonServer {
 			Map<String, List<Double>> result = coordinates(id);
 			puts("Got list");
 			puts(result);
-/*			List<Double> results = ProgressionCalculator.calculate(result.get("xs"), result.get("ys"));
+			List<Double> results = ProgressionCalculator.calculate(result.get("xs"), result.get("ys"));
 			puts(results);
 			send_to_server(id, results);
-			*/
 		}
 	}
+	*/
 	
 	public static void puts(Object arg){
 		System.out.println(arg);
 	}
 
 	public static List<Long> list_needs_evaluation() throws ClientProtocolException, IOException{
-		Response response = Request.Get(BASE_URL+"?api_key="+API_KEY).execute();
+		Response response = Request.Get(System.getProperty("rehab.server.url")+"?api_key="+System.getProperty("rehab.server.api")).execute();
 		String json_string = response.returnContent().asString();
 		//puts(json_string);
 		Object obj = JSONValue.parse(json_string);
@@ -67,7 +64,7 @@ public class JsonServer {
 	}
 
 	public static Map<String, List<Double>> coordinates(long id) throws ClientProtocolException, IOException{
-		/*Response response = Request.Get(BASE_URL+"/"+id+"?api_key="+API_KEY).execute();
+		Response response = Request.Get(System.getProperty("rehab.server.url")+"/"+id+"?api_key="+System.getProperty("rehab.server.api")).execute();
 		String json_string = response.returnContent().asString();
 		JSONObject obj = (JSONObject)JSONValue.parse(json_string);
 		List<Double> ys = new ArrayList<Double>();
@@ -80,18 +77,17 @@ public class JsonServer {
 		for(Object value : json){
 			xs.add(((Long)value).doubleValue());
 		}
-		*/
 		//JSONArray array = (JSONArray)obj;
-		List<Double> ys = asList(3d, 3d, 3d, 2d, 1d, 4d, 2d, 1d, 4d, 2d, 1d, 4d, 2d, 1d, 3d, 5d, 5d, 5d, 1d, 1d, 1d, 1d, 1d, 1d, 5d, 5d, 5d, 5d, 5d, 5d, 5d, 5d, 5d, 1d, 1d, 1d, 1d, 1d, 1d, 1d, 1d, 1d, 1d, 1d, 1d, 1d, 1d, 1d);
-		List<Double> xs = asList(1402923820d, 1402923820d, 1402923820d, 1402927643d, 1402927643d, 1402927643d, 1402927930d, 1402927930d, 1402927930d, 1402928536d, 1402928536d, 1402928536d, 1402929357d, 1402929357d, 1402929357d, 1403011392d, 1403011392d, 1403011392d, 1403011470d, 1403011470d, 1403011470d, 1403011500d, 1403011500d, 1403011500d, 1403012929d, 1403012929d, 1403012929d, 1403012988d, 1403012988d, 1403012988d, 1403013452d, 1403013452d, 1403013452d, 1403013474d, 1403013474d, 1403013474d, 1403013534d, 1403013534d, 1403013534d, 1403013573d, 1403013573d, 1403013573d, 1403013596d, 1403013596d, 1403013596d, 1403013625d, 1403013625d, 1403013625d);
+//		List<Double> ys = asList(3d, 3d, 3d, 2d, 1d, 4d, 2d, 1d, 4d, 2d, 1d, 4d, 2d, 1d, 3d, 5d, 5d, 5d, 1d, 1d, 1d, 1d, 1d, 1d, 5d, 5d, 5d, 5d, 5d, 5d, 5d, 5d, 5d, 1d, 1d, 1d, 1d, 1d, 1d, 1d, 1d, 1d, 1d, 1d, 1d, 1d, 1d, 1d);
+//		List<Double> xs = asList(1402923820d, 1402923820d, 1402923820d, 1402927643d, 1402927643d, 1402927643d, 1402927930d, 1402927930d, 1402927930d, 1402928536d, 1402928536d, 1402928536d, 1402929357d, 1402929357d, 1402929357d, 1403011392d, 1403011392d, 1403011392d, 1403011470d, 1403011470d, 1403011470d, 1403011500d, 1403011500d, 1403011500d, 1403012929d, 1403012929d, 1403012929d, 1403012988d, 1403012988d, 1403012988d, 1403013452d, 1403013452d, 1403013452d, 1403013474d, 1403013474d, 1403013474d, 1403013534d, 1403013534d, 1403013534d, 1403013573d, 1403013573d, 1403013573d, 1403013596d, 1403013596d, 1403013596d, 1403013625d, 1403013625d, 1403013625d);
 		Map<String, List<Double>> result = new HashMap<String, List<Double>>();
 		result.put("xs", xs);
 		result.put("ys", ys);
 		return result;
 	}
 
-	public static void send_to_server(long id, List<Double> results) throws ClientProtocolException, IOException{
-		String url = BASE_URL+"/"+id+"/result?api_key="+API_KEY+"&value="+results.get(0)+"&slope="+results.get(1)+"&recommendation="+results.get(2);
+	public static void send_to_server(long id, double estimate, double slope, double recommendation ) throws ClientProtocolException, IOException{
+		String url = System.getProperty("rehab.server.url")+"/"+id+"/result?api_key="+System.getProperty("rehab.server.api")+"&value="+estimate+"&slope="+slope+"&recommendation="+recommendation;
 		String status = Request.Get(url).execute().returnResponse().toString();
 		puts("Done "+id+" with "+status);
 	}
--- src/main/java/dk/medware/rehab/SCaller.java
+++ src/main/java/dk/medware/rehab/SCaller.java
@@ -3,7 +3,6 @@ package dk.medware.rehab;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,66 +12,32 @@ import org.universAAL.middleware.service.CallStatus;
 import org.universAAL.middleware.service.ServiceCaller;
 import org.universAAL.middleware.service.ServiceRequest;
 import org.universAAL.middleware.service.ServiceResponse;
-import org.universAAL.ontology.phThing.DeviceService;
 import org.universAAL.ontology.rehabontology.ExerciseAnalyser;
 import org.universAAL.ontology.rehabontology.ExerciseResults;
-import org.universAAL.ontology.rehabontology.RehabontologyOntology;
-import org.universAAL.ontology.device.SwitchController;
+import org.universAAL.ontology.rehabontology.SuggestionResult;
 
 public class SCaller extends ServiceCaller {
 
-	/* -Example- This namespace should only be used here */
-	private static final String CLIENT_OWN_NAMESPACE = "http://uaal.medware.dk/rehab/caller.owl#"; //  "http://your.ontology.URL.com/YourClientDomainOntology.owl#";
-	// TODO: Change Namespace
-	/* -Example- URI Constants for handling and identifying outputs */
+	private static final String CLIENT_OWN_NAMESPACE = "http://uaal.medware.dk/rehab/caller.owl#";
 	private static final String EXPECT_OUTPUT = CLIENT_OWN_NAMESPACE + "output";
 
 	protected SCaller(ModuleContext context) {
 		super(context);
-		// TODO Auto-generated constructor stub
 	}
 
 	public void communicationChannelBroken() {
-		// TODO Auto-generated method stub
-
 	}
 
 	public void handleResponse(String reqID, ServiceResponse response) {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * -Example- These shortcut methods allow other classes to quickly invoke
-	 * service calls and get the result. This example can call its own callee.
-	 */
-	protected Boolean callSetStatus(boolean status) {
-		ServiceResponse sr = this.call(getSetStatus(status));
-		return new Boolean(sr.getCallStatus() == CallStatus.succeeded);
-	}
-
-	protected Boolean callGetStatus() {
-		ServiceResponse sr = this.call(getGetStatus());
-		if (sr.getCallStatus() == CallStatus.succeeded) {
-			List<Object> outs = sr.getOutput(EXPECT_OUTPUT);
-			if (outs == null || outs.isEmpty()) {
-				return null;
-			} else {
-				return (Boolean) outs.get(0);
-			}
-		} else {
-			return null;
-		}
 	}
 
 	protected Boolean callGetExerciseSuggestions() throws ClientProtocolException, IOException{
-		JsonServer.service_server();
 		List<Long> ids = JsonServer.list_needs_evaluation(); 
-		//System.out.println(ids);
-		ids.add(1l);
 		for(long id : ids){
 			Map<String, List<Double>> coordinates = JsonServer.coordinates(id);
-			
+			if(coordinates.get("ys").size() <= 0) {
+				continue;
+			}
 			ServiceRequest setExerciseResults = new ServiceRequest(new ExerciseAnalyser(), null);
 			ArrayList<ExerciseResults> results = new ArrayList<ExerciseResults>();
 			ExerciseResults result = new ExerciseResults();
@@ -83,35 +48,14 @@ public class SCaller extends ServiceCaller {
 			setExerciseResults.addRequiredOutput(EXPECT_OUTPUT, new String[] {
 					ExerciseAnalyser.PROP_SUGGESTION_RESULT });
 			ServiceResponse sr = this.call(setExerciseResults);
-			//System.out.println("Call status: " + sr.getCallStatus());
-			System.out.println("Call response: "+ sr.getOutput(EXPECT_OUTPUT));
+			if(sr.getCallStatus() == CallStatus.succeeded){
+				SuggestionResult suggestion = (SuggestionResult) sr.getOutput(EXPECT_OUTPUT).get(0);
+				JsonServer.send_to_server(id, suggestion.getEstimate() , suggestion.getSlope(), suggestion.getRecommendation());
+			} else {
+				System.out.println("UAAL Call status: " + sr.getCallStatus());
+			}
 		}
 		return true;
-	}
-
-
-	/*
-	 * -Example- These "get" methods elaborate the Service Request for each
-	 * appropriate call
-	 */
-	private ServiceRequest getSetStatus(boolean status) {
-		// This ServiceRequest matches the first profile provided by the callee
-		ServiceRequest setStatus = new ServiceRequest(new DeviceService(), null);
-		setStatus.addValueFilter(new String[] { DeviceService.PROP_CONTROLS },
-				new SwitchController(CPublisher.DEVICE_OWN_URI));
-		setStatus.addChangeEffect(new String[] { DeviceService.PROP_CONTROLS,
-				SwitchController.PROP_HAS_VALUE }, new Boolean(status));
-		return setStatus;
-	}
-
-	private ServiceRequest getGetStatus() {
-		// This ServiceRequest matches the second profile provided by the callee
-		ServiceRequest setStatus = new ServiceRequest(new DeviceService(), null);
-		setStatus.addValueFilter(new String[] { DeviceService.PROP_CONTROLS },
-				new SwitchController(CPublisher.DEVICE_OWN_URI));
-		setStatus.addRequiredOutput(EXPECT_OUTPUT, new String[] {
-				DeviceService.PROP_CONTROLS, SwitchController.PROP_HAS_VALUE });
-		return setStatus;
 	}
 
 }
